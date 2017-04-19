42b99ae28026ae59440655284860aac267c238fd
Michael Hoot <mph28@peacock.zoo.cs.yale.edu>
2017-04-18 22:03:18 -0400

    Added reader that allows reading in TGF (trivial graph format) files

--- lib/graph_test.go
+++ lib/graph_test.go
@@ -43,3 +43,36 @@ func TestTelecomGraph(t *testing.T) {
    println("PASS: Graph test")
 }
 
+func TestTGFReader(t *testing.T) {
+    graph, _ := readGraph("tgf_example.tgf")
+
+    nodeList := [6]AgencyPair {
+        AgencyPair{"1234567890", 0},
+        AgencyPair{"1234567891", 1},
+        AgencyPair{"1234567892", 2},
+        AgencyPair{"1234567893", 3},
+        AgencyPair{"1234567894", 4},
+        AgencyPair{"1234567895", 5},
+    }
+
+    for _, val := range nodeList {
+        if !graph.ContainsNode(val) {
+            panic("ERROR:  graph doesn't contain node")
+        }
+    }
+
+    println("PASS: TGF node test")
+
+    if !graph.ContainsEdge(nodeList[0], nodeList[1]) ||
+       !graph.ContainsEdge(nodeList[1], nodeList[3]) ||
+       !graph.ContainsEdge(nodeList[1], nodeList[2]) ||
+       !graph.ContainsEdge(nodeList[5], nodeList[4]) ||
+       !graph.ContainsEdge(nodeList[1], nodeList[0]) ||
+       !graph.ContainsEdge(nodeList[3], nodeList[1]) ||
+       !graph.ContainsEdge(nodeList[2], nodeList[1]) ||
+       !graph.ContainsEdge(nodeList[4], nodeList[5]) {
+            panic("ERROR: Graph doesn't contain edge")
+   }
+
+    println("PASS: TGF edge test")
+}
--- lib/telecom_graph.go
+++ lib/telecom_graph.go
@@ -1,5 +1,13 @@
 package lib
 
+import (
+    "os"
+    "bufio"
+    "strings"
+    "strconv"
+    "fmt"
+)
+
 type AgencyPair struct {
     Node        string
     Telecom     int
@@ -13,22 +21,30 @@ type Edge struct {
 type TelecomGraph struct {
     NumNodes    int
     Nodes       map[AgencyPair]bool
+    telecoms    map[string]int
     Visited     map[AgencyPair]bool
     Graph       map[AgencyPair][]Edge
 }
 
 func NewGraph(nodeList []AgencyPair) *TelecomGraph {
     contains := make(map[AgencyPair]bool)
+    tcoms := make(map[string]int)
     for _, item := range nodeList {
         contains[item] = true
+        tcoms[item.Node] = item.Telecom
     }
 
     return &TelecomGraph {
         NumNodes:   len(nodeList),
         Nodes:      contains,
+        telecoms:   tcoms,
         Visited:    make(map[AgencyPair]bool),
         Graph:      make(map[AgencyPair][]Edge),
     }
+}
+
+func (g *TelecomGraph) Telecom (phone string) int {
+    return g.telecoms[phone]
 }
 
 func (g *TelecomGraph) ContainsNode(node AgencyPair) bool {
@@ -72,4 +88,46 @@ func (g *TelecomGraph) AddNode(node AgencyPair) {
 
     g.NumNodes++
     g.Nodes[node] = true
+}
+
+func readGraph (path string) (*TelecomGraph, error) {
+	file, err := os.Open(path)
+	if err != nil {
+		fmt.Printf("%v\n", err)
+		return nil, err
+	}
+	defer file.Close()
+
+    var list []AgencyPair
+    scanner := bufio.NewScanner(file)
+
+    // First half of the TGF format:  Node definitions
+    for scanner.Scan() {
+
+        // Check for seperator
+        if scanner.Text() == "#" {
+            break
+        }
+
+        // Otherwise split into its components and form a pair
+        data := strings.Fields(scanner.Text())
+        phone := data[0]
+        telecom, _ := strconv.Atoi(data[1])
+        list = append(list, AgencyPair{phone, telecom})
+    }
+
+    graph := NewGraph(list)
+
+    // Second half of TGF format: Edge definitions
+    for scanner.Scan() {
+
+        data := strings.Fields(scanner.Text())
+        node1 := AgencyPair{data[0], graph.Telecom(data[0])}
+        node2 := AgencyPair{data[1], graph.Telecom(data[1])}
+        weight, _ := strconv.Atoi(data[2])
+
+        graph.AddEdge(node1, node2, weight)
+    }
+
+    return graph, scanner.Err()
 }
